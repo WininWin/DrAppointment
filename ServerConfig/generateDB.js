84e629d0ce1bcf09fb1ee0a5f6f7bf92dd2af80f
Machine <ezoneid@gmail.com>
2018-01-14 19:48:42 -0600

    sample data

--- ServerConfig/generateDB.js
+++ ServerConfig/generateDB.js
@@ -14,7 +14,7 @@ const testPassword = '12345678';
 const bcrypt = require('bcrypt-nodejs');
 const fs = require('fs');
 const mkdirp = require('mkdirp');
-const dir = __dirname  + '/../medicalRecords';
+const dir = __dirname  + '/../uploadSample';
 
 
 function randomDate(start, end) {
